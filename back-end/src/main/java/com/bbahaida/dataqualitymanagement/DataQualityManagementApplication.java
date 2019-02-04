bde22e0a20ed05ba330d98867780503901517219
Brahim Bahaida <bbahieda@gmail.com>
2019-02-04 09:42:39 +0100

    structure(backend): project structure
    add the project initial structure and dependencies

---
+++ back-end/src/main/java/com/bbahaida/dataqualitymanagement/DataQualityManagementApplication.java
@@ -0,0 +1,14 @@
+package com.bbahaida.dataqualitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataQualityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataQualityManagementApplication.class, args);
+	}
+
+}
+
